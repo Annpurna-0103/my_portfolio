fdf2af757c0c17d89d16c022f6365ee630312818
Annpurna-0103 <annutripathi0112@gmail.com>
2025-02-06 15:09:58 +0530

    initial commit

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,7 @@
-pip install -r requirements.txt
-python manage.py collectstatic
+# Install dependencies 
+echo "Installing dependencies..." 
+pip3 install -r requirements.txt 
+ 
+# Run Django collectstatic to gather static files before deployment 
+echo "Running collectstatic..." 
+python3 manage.py collectstatic --noinput
